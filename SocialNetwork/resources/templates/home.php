9fc9e1784a669471d1ffb5a723036f6539dedfe0
Jakob Bussas <jakobbu@gmx.de>
2017-04-14 16:46:47 +0200

    Sogar das LoginFormular wird noch angezeigt.

--- SocialNetwork/resources/templates/home.php
+++ SocialNetwork/resources/templates/home.php
@@ -1,5 +1,18 @@
 <div id="home">
 
-//zeige die letzten 10 posts von freunden an
+<p>Hallo Welt</p>
+<p>Hier sollen die letzten 10 Posts von Freunden angezeigt werden</p>
 
+<?php
+$loggedin = getLoginStatus(session_id());
+
+if($loggedin) {
+	$username = getUserName(session_id());
+} else {
+	?>
+	<p>Bitte loggen Sie sich zuerst ein.</p>
+	<p><a href="?page=login">Zum Login</a></p>
+	<?php
+}
+?>
 </div>
