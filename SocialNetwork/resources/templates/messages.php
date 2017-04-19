4bc6ad6d6fd737ebb332f494ec87a027f2040437
Jakob Bussas <jakobbu@gmx.de>
2017-04-19 09:11:49 +0200

    Verlauf Anzeige eingeführt (ungerüstet)

--- SocialNetwork/resources/templates/messages.php
+++ SocialNetwork/resources/templates/messages.php
@@ -8,7 +8,27 @@ if($loggedin) {
 
 	$pdo = new PDO('mysql:host=localhost;dbname=socialnetwork', 'root', 'root');
 
-	$sql = "SELECT * FROM message";
+	$sql = "SELECT * FROM verlauf WHERE teilnehmer1 = :username OR teilnehmer2 = :username ORDER BY zeit DESC LIMIT 10";
+	$statement = $pdo->prepare($sql);
+	$statement->execute(array(':username' => $username));
+
+	while($row = $statement->fetch()) {
+		?>
+		<div class="verlauf">
+		<p><a href="#">Konversation mit 
+			<?php
+			if($row['teilnehmer1'] == $username) {
+				echo $row['teilnehmer2'];
+			} else if($row['teilnehmer2'] == $username) {
+				echo $row['teilnehmer1'];
+			}
+			//Zeigt den Namen des anderen Teilnehmers an der Konversation an, hier muss noch ein Link dahin eingeführt werden
+			?>
+		</a></p>
+
+		</div>
+		<?php
+	}
 }
 ?>
 </div>
