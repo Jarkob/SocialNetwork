f7a8a25a3ab9d87f982af8a3cd76e7a0e860f1d0
scrubkob <jakobbu@gmx.de>
2017-06-08 10:22:54 +0200

    Create sql.php
    Kleiner Fix

--- SocialNetwork/resources/classes/sql.php
+++ SocialNetwork/resources/classes/sql.php
@@ -6,24 +6,25 @@ class sql
 
 	  public static function connect($host, $username, $password, $dbname=null)
 	  {
-			//Verbindung herstellen
-			try{
-				//wenn $database übergeben wird, wird zu der Datenbank eine Verbindung hergestellt
-				$database = ';dbname=';
-				if($database != null) {
-					$database += $dbname;
-				}
-				
-				self::$pdo = new PDO('mysql:host='. $host . $database, $username, $password);
-				return;
-			}catch(PDOException $e){
-				throw new Exception($e->getMessage());
+		//Verbindung herstellen
+		try{
+			//wenn $database übergeben wird, wird zu der Datenbank eine Verbindung hergestellt
+			$database = ';dbname=';
+			if($database != null) {
+				$database += $dbname;
 			}
+			
+			self::$pdo = new PDO('mysql:host='. $host . $database, $username, $password);
+			return;
+		}catch(PDOException $e){
+			throw new Exception($e->getMessage());
+		}
 	  }
 
 	  public static function close()
 	  {
-			//Verbindung schließen
+		//Verbindung schließen
+		  
 	  }
 
 	  public static function exe()
