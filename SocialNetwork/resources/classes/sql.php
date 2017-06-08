047e2e4d3b6636fe51fbe27a13a76b4a776a50b9
scrubkob <jakobbu@gmx.de>
2017-06-08 10:21:11 +0200

    Create sql.php
    Anfang der db Klasse, connect Methode

--- SocialNetwork/resources/classes/sql.php
+++ SocialNetwork/resources/classes/sql.php
@@ -1,1 +1,40 @@
+<?php
 
+class sql
+{
+	  protected static $pdo;
+
+	  public static function connect($host, $username, $password, $dbname=null)
+	  {
+			//Verbindung herstellen
+			try{
+				//wenn $database übergeben wird, wird zu der Datenbank eine Verbindung hergestellt
+				$database = ';dbname=';
+				if($database != null) {
+					$database += $dbname;
+				}
+				
+				self::$pdo = new PDO('mysql:host='. $host . $database, $username, $password);
+				return;
+			}catch(PDOException $e){
+				throw new Exception($e->getMessage());
+			}
+	  }
+
+	  public static function close()
+	  {
+			//Verbindung schließen
+	  }
+
+	  public static function exe()
+	  {
+			//queries ausführen
+	  }
+
+	  public function lastInsertId()
+	  {
+			//lastInsertId prüfen
+	  }
+}
+
+?>
