597e332760b310e37f574ac03c1966d4e73164d3
scrubkob <jakobbu@gmx.de>
2017-06-08 10:38:56 +0200

    Parameterverarbeitung
    Anfang

--- SocialNetwork/resources/classes/sql.php
+++ SocialNetwork/resources/classes/sql.php
@@ -27,9 +27,20 @@ class sql
 		self::$pdo = null;
 	}
 
-	public static function exe()
+	//$sql ist der Befehl(keine null queries!?), $params ein assoziatives array mit den Parametern
+	public static function exe($sql, $params=null)
 	{
 		//queries ausführen
+		$statement = self::$pdo>prepare($sql);
+		
+		//bind_para ist ein Platzhalter für z.b. Limit Sachen,
+		//die brauchen nämlich einen Integer es werden aber defaultmäßig
+		//nur strings übergeben
+		if(($params !== null) && (strpos($sql, ' LIMIT:') !== false) || (strpos($sql, ' limit:') !== false) {
+			$bind_para = true;
+		} else {
+			$bind_para = false;
+		}
 	}
 
 	public function lastInsertId()
