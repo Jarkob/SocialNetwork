88ede217afaab6538405981353de1b80a2fd7ec0
Jakob Bussas <jakobbu@gmx.de>
2017-06-09 19:39:15 +0200

    ok

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -3,8 +3,8 @@ session_start();
 
 require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
 
-require_once(LIBRARY_PATH . "/templateFunctions.php");
-global $pdo;
+//require_once(LIBRARY_PATH . "/templateFunctions.php");
+//global $pdo;
 //renderLayoutWithContentFile($pdo);
 
 ?>
