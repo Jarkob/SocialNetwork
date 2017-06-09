d531233731e2cb1009395b5e11770f2f8f5a3cdd
Jakob Bussas <jakobbu@gmx.de>
2017-06-09 21:29:12 +0200

    Keine Ahnung, ob das jemals funktionieren wird

--- SocialNetwork/resources/classes/entry.php
+++ SocialNetwork/resources/classes/entry.php
@@ -5,8 +5,19 @@ class entry
 	public static createNewEntry($autor, $content)
 	{
 		$sql = "INSERT INTO entry
-			()
-			VALUES()";
+			(content, autor)
+			VALUES(:content, :autor)";
+		$params = {"content" => $content, "autor" => $autor}
+		sql::exe($sql, $params);
+	}
+
+	public static getEntryById($id)
+	{
+		$sql = "SELECT * FROM entry
+			WHERE id = :id";
+		$params = {"id" => $id}
+		$entry = sql::exe($sql, $params);
+		return $entry;
 	}
 }
 
