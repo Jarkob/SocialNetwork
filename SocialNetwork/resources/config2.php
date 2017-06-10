179f324ad43b4a944071dba979559bbd86f09476
Jakob Bussas <jakobbu@gmx.de>
2017-06-10 12:27:20 +0200

    Neue config

---
+++ SocialNetwork/resources/config2.php
@@ -0,0 +1,15 @@
+<?php
+
+defined("CLASSES_PATH")
+	or define("CLASSES_PATH", realpath(dirname(__FILE__) ."/classes"));
+
+defined("VIEWS_PATH")
+	or define("VIEWS_PATH", realpath(dirname(__FILE__) ."/views"));
+
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRICT);
+
+require_once(CLASSES_PATH ."/sql.php");
+sql::connect();
+
+?>
