5cad57573e3cc81be9f2fe73a050d570ac157258
Jakob Bussas <jakobbu@gmx.de>
2017-06-10 18:35:25 +0200

    fix

--- SocialNetwork/resources/library/templateFunctions2.php
+++ SocialNetwork/resources/library/templateFunctions2.php
@@ -3,10 +3,10 @@
 function renderPage()
 {
 	//hier kommt das skript an
-	require_once(VIEWS_PATH ."header.view.php");
+	require_once(VIEWS_PATH ."/header.view.php");
 
 	$view = VIEWS_PATH;
-echo "nutten";
+
 	if(isset($_GET['page'])) {
 		switch($_GET['page']) {
 			case 'nutten':
@@ -15,12 +15,12 @@ echo "nutten";
 				break;
 		}
 	} else {
-		$view = $view . 'home.view.php';
+		$view = $view . '/home.view.php';
 	}
 
 	require_once($view);
 
-	require_once(VIEWS_PATH ."footer.view.php");
+	require_once(VIEWS_PATH ."/footer.view.php");
 }
 
 ?>
