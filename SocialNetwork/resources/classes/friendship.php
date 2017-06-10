58f9a0d220dc26f26a49d44ddad90a4054d5c750
Jakob Bussas <jakobbu@gmx.de>
2017-06-10 09:51:35 +0200

    Friendship Klasse erstellt.

---
+++ SocialNetwork/resources/classes/friendship.php
@@ -0,0 +1,42 @@
+<?php
+
+class friendship
+{
+	protected $id;
+	protected $freund1;
+	protected $freund2;
+
+	public static function createNewFriendship($freund1, $freund2)
+	{
+		$sql = "INSERT INTO friendship
+			(freund1, freund2)
+			VALUES (:freund1, :freund2)";
+		$params = {"freund1" => $freund1, "freund2" => $freund2}
+		sql::exe($sql, $params);
+	}
+
+	public static function checkFriendship($freund1, $freund2)
+	{
+		$sql = "SELECT * FROM friendship
+			WHERE (freund1 = :freund1 AND freund2 = :freund2)
+			OR (freund1 = :freund2 AND freund2 = :freund1)";
+		$params = {"freund1" => $freund1, "freund2" => $freund2}
+		$result = sql::exe($sql, $params);//tricky, könnte falsch sein
+		if(sizeof($result) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function deleteFriendship()
+	{
+		$sql = "DELETE FROM friendship
+			WHERE id = :id";
+		$params = {"id" => $this->id}
+		sql::exe($sql, $params);
+		settype(&$this, 'null');
+	}
+}
+
+?>
