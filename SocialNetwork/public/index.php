bd8257f88b8156132b4efed61a1f96b9f0f19e0e
Jakob Bussas <jakobbu@gmx.de>
2017-06-12 19:01:45 +0200

    test

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -2,7 +2,7 @@
 session_start();
 echo "nutten";
 require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
-//require_once(LIBRARY_PATH . "/templateFunctions.php");
+require_once(LIBRARY_PATH . "/templateFunctions.php");
 global $pdo;
 //renderLayoutWithContentFile($pdo);
 
