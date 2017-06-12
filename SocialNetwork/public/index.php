661a03853785310a3a41a4094ce4bce1e355a665
Jakob Bussas <jakobbu@gmx.de>
2017-06-12 19:02:15 +0200

    fix

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-echo "nutten";
+
 require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
 require_once(LIBRARY_PATH . "/templateFunctions.php");
 global $pdo;
-//renderLayoutWithContentFile($pdo);
+renderLayoutWithContentFile($pdo);
 
 ?>
