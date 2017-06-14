7f46de38787dcda1695297bb0948b23710e328dd
scrubkob <jakobbu@gmx.de>
2017-06-14 11:40:01 +0200

    debug

--- SocialNetwork/public/index2.php
+++ SocialNetwork/public/index2.php
@@ -5,5 +5,6 @@ session_start();
 require_once(realpath(dirname(__FILE__) ."/../resources/config2.php"));
 echo "Hallo Welt";
 require_once(LIBRARY_PATH ."/templateFunctions2.php");
+echo "vor dem Aufruf der rendepagefunktion"
 renderPage();
 ?>
