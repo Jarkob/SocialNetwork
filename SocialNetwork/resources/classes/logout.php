c7bfbae95019eff802b2c4af28ed12059f4a4c93
Jakob Bussas <jakobbu@gmx.de>
2017-06-16 19:36:37 +0200

    debug

---
+++ SocialNetwork/resources/classes/logout.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(realpath(dirname(__FILE_) ."/../config2.php"));
+require_once(CLASSES_PATH ."/sql.php");
+require_once(CLASSES_PATH ."/user.php");
+
+class logout
+{
+	public static function logoutUser(user $user)
+	{
+		$sql = "UPDATE user SET sid = ";
+	}
+}
+
+?>
