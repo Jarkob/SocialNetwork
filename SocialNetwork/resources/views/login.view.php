48bad7b21ac3a7a0083e8a1c7d538b43fe27225a
Jakob Bussas <jakobbu@gmx.de>
2017-06-16 12:03:10 +0200

    vergessen

---
+++ SocialNetwork/resources/views/login.view.php
@@ -0,0 +1,48 @@
+<div id="login">
+<?php
+require_once(CLASSES_PATH ."/user.php");
+require_once(CLASSES_PATH ."/login.php");
+if(login::isLoggedIn(session_id())) {
+?>
+	<p>Sie sind bereits eingeloggt.</p>
+<?php
+} else {
+	if(array_key_exists('username', $_POST) && array_key_exists('password', $_POST)) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		if(login::loginUser($username, $password)) {
+		?>
+			<p>Sie sind nun eingeloggt.</p>
+			<script type="text/javascript">
+				document.location.href = "index2.php";
+			</script>
+			<p><a href="index2.php">Zurück zur Startseite</a></p>
+		<?php
+		} else {
+		?>
+			<p>Ihre Logindaten waren nicht korrekt.</p>
+			<p><a href="#">Nochmal versuchen</a></p>
+		<?php	
+		}
+	} else {
+	?>
+		<form action="" method="post">
+			<div>
+				<p><label for="username">Username</label></p>
+				<p><input id="username" name="username"></p>
+			</div>
+			<div>
+				<p><label for="password">Passwort</label></p>
+				<p><input id="password" name="username" type="password"></p>
+			</div>
+			<p><button>Anmelden</button></p>
+		</form>
+		<p>Besitzen Sie noch keinen Account?</p>
+		<p>Dann können Sie sich <a href="#">hier</a> registrieren.</p>
+
+	<?php
+	}
+}
+?>
+</div>
