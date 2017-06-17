6da2af4d63c766940a7b63869d58c9eec89ddd39
Jakob Bussas <jakobbu@gmx.de>
2017-06-17 16:54:28 +0200

    debug

--- SocialNetwork/resources/views/logout.view.php
+++ SocialNetwork/resources/views/logout.view.php
@@ -3,7 +3,7 @@
 require_once(CLASSES_PATH ."/user.php");
 require_once(CLASSES_PATH ."/login.php");
 require_once(CLASSES_PATH ."/logout.php");
-
+echo "<h1>Alles requiren ging</h1>";
 if(login::isLoggedIn(session_id())) {
 	$user = user::findUserBySid(session_id());
 	$user->changeSid('loggedOut');
