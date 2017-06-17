40ac60f5e781b8ce7895e21ef853969331a613e5
Jakob Bussas <jakobbu@gmx.de>
2017-06-17 16:53:03 +0200

    fix

--- SocialNetwork/resources/views/logout.view.php
+++ SocialNetwork/resources/views/logout.view.php
@@ -5,7 +5,7 @@ require_once(CLASSES_PATH ."/login.php");
 require_once(CLASSES_PATH ."/logout.php");
 
 if(login::isLoggedIn(session_id())) {
-	$user = user::getUserBySid(session_id());
+	$user = user::findUserBySid(session_id());
 	$user->changeSid('loggedOut');
 ?>
 	<p>Sie wurden ausgeloggt.</p>
