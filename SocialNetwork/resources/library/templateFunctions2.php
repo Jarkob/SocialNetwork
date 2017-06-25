c69a907be4e194571f50a9d032123255582ed5e5
Jakob Bussas <jakobbu@gmx.de>
2017-06-25 17:54:05 +0200

    debug

--- SocialNetwork/resources/library/templateFunctions2.php
+++ SocialNetwork/resources/library/templateFunctions2.php
@@ -1,4 +1,5 @@
 <?php
+require_once(realpath(dirname(__FILE__) ."/../config2.php"));
 require_once(CLASSES_PATH ."/sql.php");
 require_once(CLASSES_PATH ."/user.php");
 require_once(CLASSES_PATH ."/entry.php");
