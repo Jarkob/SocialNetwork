56ee3518d3c29629b8326cbf279b8a778e6c3f23
Jakob Bussas <jakobbu@gmx.de>
2017-06-29 21:02:05 +0200

    Seitenanzeige

--- SocialNetwork/resources/library/templateFunctions2.php
+++ SocialNetwork/resources/library/templateFunctions2.php
@@ -43,9 +43,28 @@ function renderHome()
 		$sql .= "' OR autor = '". $friend;
 	}
 	$sql .= "' ORDER BY zeit DESC LIMIT 10";
-	echo $sql;
 
 	$result = sql::exe($sql);
+
+
+	// Einträge zählen
+	$anzahlEintraege = sizeof($result);
+	$anzahlProSeite = 10;
+	$anzahlSeiten = $anzahlEintraege / $anzahlProSeite;
+
+	if(empty($_GET['nr'])) {
+		$seite = 1;
+	} else {
+		$seite = $_GET['nr'];
+		if($seite > $anzahlSeiten) {
+			$seite = 1;
+		}
+	}
+	$start = ($seite * $anzahlProSeite) - $anzahlProSeite;
+	$limit = $start + 10;
+
+
+
 	$entries = array();
 	
 	for($i = 0; $i < sizeof($result); $i++) {
@@ -55,6 +74,28 @@ function renderHome()
 	foreach($entries as $entry) {
 		$entry->renderEntry();
 	}
+
+	?>
+	<ul id="seiten">
+		<?php
+		for($i = 1; $i <= $anzahlSeiten; $i++) {
+			if($seite == $i) {
+				?>
+				<li>
+					<a href="?page=home&nr=<?= $i?>"><b>Seite <?= $i?></b></a>
+				</li>
+				<?php
+			} else {
+				?>
+				<li>
+					<a href="?page=home&nr=<?= $i?>">Seite <?= $i?></a>
+				</li>
+				<?php
+			}
+		}
+		?>
+	</ul>
+	<?php
 }
 
 ?>
