5c4d1cd04cd71f5968a23bde55cb2607a8f71a61
Jakob Bussas <jakobbu@gmx.de>
2017-07-01 15:50:08 +0200

    debug

--- SocialNetwork/resources/views/newEntry.view.php
+++ SocialNetwork/resources/views/newEntry.view.php
@@ -8,18 +8,20 @@
 </div>
 
 <?php
-//hier ist irgendwo der Wurm drin
-echo "vor dem requiren in der newentry.view.php";
+
 require_once(CLASSES_PATH ."/sql.php");
 require_once(CLASSES_PATH ."/user.php");
 require_once(CLASSES_PATH ."/entry.php");
-echo "requiren klappt";
+// Wurm ab hier
 if(array_key_exists('content', $_POST)) {
-
+	echo "if abfrage erreicht";
 	$username = user::findUserBySid(session_id());
-
+	echo "Username gegettet";
 	$entry = new entry($username, $_POST['content']);
+	echo "neuer entry konstruiert";
 	$entry->createNewEntry();
+	echo "neuer entry gedatenbankt";
 }
+echo "nach if abfrage";
 
 ?>
