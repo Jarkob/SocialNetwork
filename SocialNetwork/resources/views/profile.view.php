bf35cf6cd360348e9cb0d9fe7f13419a4abd56ca
Jakob Bussas <jakobbu@gmx.de>
2017-07-02 16:07:44 +0200

    debug

--- SocialNetwork/resources/views/profile.view.php
+++ SocialNetwork/resources/views/profile.view.php
@@ -1,13 +1,17 @@
 <?php
+echo "in der profile.view";
 require_once(CLASSES_PATH ."/user.php");
 require_once(CLASSES_PATH ."/profile.php");
-
+echo "nach dem requiren";
 if(isset($_GET['owner'])) {
 	$owner = $_GET['owner'];
 } else {
 	$owner = user::findUserBySid(session_id());
 }
+echo "der owner ist ". $owner;
 
 $profile = new profile(user::findUserByUsername($owner));
+echo "nach profilinitialisierung";
 $profile->renderProfile();
+echo "nach profilladen";
 ?>
