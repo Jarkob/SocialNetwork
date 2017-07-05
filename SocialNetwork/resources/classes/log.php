0970ebbe4ed01fcd647993676e227cd916e3dffe
Jakob Bussas <jakobbu@gmx.de>
2017-07-05 20:22:48 +0200

    debug

--- SocialNetwork/resources/classes/log.php
+++ SocialNetwork/resources/classes/log.php
@@ -4,17 +4,18 @@ $ip = getenv('REMOTE_ADDR');
 $userAgent = getenv('HTTP_USER_AGENT');
 $referrer = getenv('HTTP_REFERER');
 $query = getenv('QUERY_STRING');
-
+/*
 echo $ip ."<br>";
 echo $userAgent ."<br>";
 echo $referrer ."<br>";
 echo $query;
-
+*/
 //$sql = "INSERT INTO access_log (ip, browser) VALUES (:ip, :browser)";
 
-
+/*
 class log
 {
 
 }
+*/
 ?>
