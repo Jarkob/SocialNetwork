16e29c0442ca674847681b1b9f018a5e5b449b2d
Jakob Bussas <jakobbu@gmx.de>
2017-07-05 21:48:16 +0200

    oo

--- SocialNetwork/resources/views/sendFriendrequest.view.php
+++ SocialNetwork/resources/views/sendFriendrequest.view.php
@@ -4,8 +4,8 @@ require_once(CLASSES_PATH ."/friendrequest.php");
 
 if(isset($_GET['id'])) {
 	$username = user::findUserBySid(session_id());
-	friendrequest::createNewFriendrequest($username, $_GET['id']);
-
+	$user = new user($username);
+	$user->sendFriendrequest($_GET['id']);
 	?>
 	<p>Sie haben <?= $_GET['id']?> eine Freundschaftsanfrage gesendet.</p>
 	<?php
