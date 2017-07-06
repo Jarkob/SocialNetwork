c5ae6c0aa03ccddf658f2461ef722d294cefae10
Jakob Bussas <jakobbu@gmx.de>
2017-07-06 17:13:45 +0200

    debug

--- SocialNetwork/resources/classes/friendship.php
+++ SocialNetwork/resources/classes/friendship.php
@@ -14,6 +14,7 @@ class friendship
 
 	public static function createNewFriendship($freund1, $freund2)
 	{
+		echo "in createNewFriendship Funktion";
 		$sql = "INSERT INTO friendship
 			(freund1, freund2)
 			VALUES (:freund1, :freund2)";
