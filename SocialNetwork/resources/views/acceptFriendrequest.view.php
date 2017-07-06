e79f2faac11b621a22108316b33ee41247e4c15d
Jakob Bussas <jakobbu@gmx.de>
2017-07-06 17:03:29 +0200

    debug

--- SocialNetwork/resources/views/acceptFriendrequest.view.php
+++ SocialNetwork/resources/views/acceptFriendrequest.view.php
@@ -1,12 +1,15 @@
 <?php
+echo "vor requiren";
 require_once(CLASSES_PATH ."/user.php");
 require_once(CLASSES_PATH ."/friendrequest.php");
-
+echo "nach requiren";
 if(isset($_GET['id'])) {
+	echo "in if";
 	$username = user::findUserBySid(session_id());
 	$user = new user($username);
-
+	echo "nach userinitialisierung";
 	$friendrequest = new friendrequest($_GET['id']);
+	echo "nach Freundschaftsanfragen initialisierung";
 	if($friendrequest->getEmpfaenger() == $username) {
 		$user->acceptFriendrequest($friendrequest);
 		?>
