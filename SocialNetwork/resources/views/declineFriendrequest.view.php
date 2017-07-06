41eaa2db7c2af55831d89db992bfd258fa09b6ad
Jakob Bussas <jakobbu@gmx.de>
2017-07-06 17:29:40 +0200

    fix

--- SocialNetwork/resources/views/declineFriendrequest.view.php
+++ SocialNetwork/resources/views/declineFriendrequest.view.php
@@ -1,6 +1,6 @@
 <?php
 require_once(CLASSES_PATH ."/user.php");
-require_once(CLASSES_PATH ."/friendreques.php");
+require_once(CLASSES_PATH ."/friendrequest.php");
 
 if(isset($_GET['id'])) {
 	$username = user::findUserBySid(session_id());
