153e984c3bc637443658a43680171bbdfcbbfca7
Jakob Bussas <jakobbu@gmx.de>
2017-07-11 15:57:02 +0200

    Registrierung Grundgerüst

--- SocialNetwork/resources/views/registration.view.php
+++ SocialNetwork/resources/views/registration.view.php
@@ -2,9 +2,33 @@
 
 // Wenn die Parameter gesetzt sind wird der Registrierungsprozess gestartet, ansonsten wird das Formular angezeigt
 if(isset($_POST['newUserName'], $_POST['newPassword'], $_POST['repeatNewPassword'])) {
+	require_once(CLASSES_PATH ."/user.php");
+
 	// Validitätsprüfung
 	$username = $_POST['newUserName'];
-
+	$user = user::findUserByUserName($username);
+	if(sizeof($user) != 0) {
+		?>
+		<p>Der gewünschte username ist bereits vergeben.</p>
+		<?php
+	} else {
+		if($_POST['newPassword'] != $_POST['repeatNewPassword']) {
+			?>
+			<p>Die beiden Passwörter müssen übereinstimmen.</p>
+			<?php
+		} else {
+			$password = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
+			$sql = "INSERT INTO user (username, passwort) VALUES (:username, :passwort)";
+			$params = array(":username" => $username, ":passwort" => $password);
+			sql::exe($sql, $params);
+			?>
+			<p>Sie haben sich erfolgreich registriert.</p>
+			<p>
+				<a href="?page=login">Zum Login</a>
+			</p>
+			<?php
+		}
+	}
 } else {
 ?>
 <form action="?page=registration" method="post">
