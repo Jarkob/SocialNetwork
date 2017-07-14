9d999660d85b7109fe442f5540db1fe8c45ef515
Jakob Bussas <jakobbu@gmx.de>
2017-07-14 17:37:37 +0200

    debug

--- SocialNetwork/public/js/ajax/getLikes.php
+++ SocialNetwork/public/js/ajax/getLikes.php
@@ -4,10 +4,11 @@ echo "Bis hierhin funktioniert es";
 
 $sql = "SELECT * FROM entry_gefaelltMir WHERE gefallender_entry = :entryId";
 $params = array(":entryId" => $_GET['id']);
-echo "nach initialisierung undso";
-/*
-$results = sql::exe($sql, $params);
+echo "nach initialisierung";
 
+$results = sql::exe($sql, $params);
+echo "nach datenbankung";
+/*
 echo "<ul>";
 foreach($results as $result) {
 	?>
