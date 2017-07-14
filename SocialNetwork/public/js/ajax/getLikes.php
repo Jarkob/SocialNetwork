e5a576f842de0cbbe6fa053d23eacfc0ee1dbc68
Jakob Bussas <jakobbu@gmx.de>
2017-07-14 17:39:26 +0200

    debug

--- SocialNetwork/public/js/ajax/getLikes.php
+++ SocialNetwork/public/js/ajax/getLikes.php
@@ -6,6 +6,9 @@ $sql = "SELECT * FROM entry_gefaelltMir WHERE gefallender_entry = :entryId";
 $params = array(":entryId" => $_GET['id']);
 echo "nach initialisierung";
 
+require_once("../../../resources/config.php");
+require_once(CLASSES_PATH ."/sql.php");
+echo "nach requiren";
 $results = sql::exe($sql, $params);
 echo "nach datenbankung";
 /*
