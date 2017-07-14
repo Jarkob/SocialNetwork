c91b61186c214bb1cded68ce600a17a2acbee4e3
Jakob Bussas <jakobbu@gmx.de>
2017-07-14 17:40:12 +0200

    fix

--- SocialNetwork/public/js/ajax/getLikes.php
+++ SocialNetwork/public/js/ajax/getLikes.php
@@ -1,17 +1,12 @@
 <?php
-echo "Bis hierhin funktioniert es";
-
 
 $sql = "SELECT * FROM entry_gefaelltMir WHERE gefallender_entry = :entryId";
 $params = array(":entryId" => $_GET['id']);
-echo "nach initialisierung";
 
 require_once("../../../resources/config.php");
 require_once(CLASSES_PATH ."/sql.php");
-echo "nach requiren";
 $results = sql::exe($sql, $params);
-echo "nach datenbankung";
-/*
+
 echo "<ul>";
 foreach($results as $result) {
 	?>
