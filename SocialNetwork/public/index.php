21c644c6940275280482dd1a23eb3c5fc3085df5
Jakob Bussas <jakobbu@gmx.de>
2017-07-16 16:29:56 +0200

    debug

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -2,9 +2,9 @@
 echo "ab angeanf";
 session_start();
 require_once(realpath(dirname(__FILE__) ."/../resources/config.php"));
-echo "nach requiren von config.php8";
-/*
+echo "nach requiren von config.php";
+
 require_once(LIBRARY_PATH ."/templateFunctions.php");
-renderPage();
-*/
+echo "nach requiren von tfunctions";
+//renderPage();
 ?>
