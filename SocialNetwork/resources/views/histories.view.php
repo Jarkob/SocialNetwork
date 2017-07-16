54c0f3d351727d82cc6da0cacb1222764a8257f1
Jakob Bussas <jakobbu@gmx.de>
2017-07-16 17:17:54 +0200

    debug

--- SocialNetwork/resources/views/histories.view.php
+++ SocialNetwork/resources/views/histories.view.php
@@ -1,13 +1,13 @@
 <h2>Nachrichten</h2>
 <?php
-echo "vor requiren";
 require_once(CLASSES_PATH ."/user.php");
-echo "nach user";
 require_once(CLASSES_PATH ."/history.php");
 echo "nach history";
 $username = user::findUserBySid(session_id());
 $user = new user($username);
+echo "nach userIni";
 $histories = $user->getHistories();
+echo "nach histories";
 // Fazit: Nachrichten auf diese Weise zu machen ist schlecht. Egal...
 if(sizeof($histories) != 0) {
 ?>
