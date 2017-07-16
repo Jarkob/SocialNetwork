abee6d8c4ba61834a8478a3137ba3cef381a3909
Jakob Bussas <jakobbu@gmx.de>
2017-07-16 17:05:29 +0200

    debug

--- SocialNetwork/resources/views/newHistory.view.php
+++ SocialNetwork/resources/views/newHistory.view.php
@@ -1,17 +1,31 @@
 <?php
 require_once(CLASSES_PATH ."/user.php");
+require_once(CLASSES_PATH ."/history.php");
+
 $username = user::findUserBySid(session_id());
 
 $friend = $_GET['friend'];
 
-$sql = "INSERT INTO verlauf (teilnehmer1, teilnehmer2) VALUES (:username, :friend)";
-$params = array(":username" => $username, ":friend" => $friend);
-sql::exe($sql, $params);
+$result = history::findHistoryByParticipating($username, $friend);
+if($result != "" && $result != null) {
+	$sql = "INSERT INTO verlauf (teilnehmer1, teilnehmer2, zeit) VALUES (:username, :friend, CURRENT_TIMESTAMP)";
+	$params = array(":username" => $username, ":friend" => $friend);
+	sql::exe($sql, $params);
 
+	?>
+	<p>
+		Sie können <?= $friend?> jetzt Nachrichten senden.
+	</p>
+	<script type="text/javascript">
+		document.location.href = "index.php?page=histories";
+	</script>
+
+	<?php
+} else {
+	?>
+	<script type="text/javascript">
+		document.location.href = "index.php?page=chat&id=<?= $result?>";
+	</script>
+	<?php
+}
 ?>
-<p>
-	Sie können <?= $friend?> jetzt Nachrichten senden.
-</p>
-<script type="text/javascript">
-	document.location.href = "index.php?page=histories";
-</script>
