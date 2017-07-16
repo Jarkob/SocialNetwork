a9756e62448041cde9b7c5101eee83ffb0f1b7a3
Jakob Bussas <jakobbu@gmx.de>
2017-07-16 16:28:31 +0200

    debug

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -1,7 +1,9 @@
 <?php
-echo "Am anfang";
-session_start();/*
+echo "ab angeanf";
+session_start();
 require_once(realpath(dirname(__FILE__) ."/../resources/config.php"));
+echo "nach requiren von config.php8";
+/*
 require_once(LIBRARY_PATH ."/templateFunctions.php");
 renderPage();
 */
