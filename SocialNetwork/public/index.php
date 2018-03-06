398448854dc80d55a44c98e26f94ea8f2af8dab3
Jakob Bussas <jakobbu@gmx.de>
2018-03-06 12:47:18 +0100

    debug

--- SocialNetwork/public/index.php
+++ SocialNetwork/public/index.php
@@ -4,6 +4,6 @@ session_start();
 echo "Test";
 require_once(realpath(dirname(__FILE__) ."/../resources/config.php"));
 
-// require_once(LIBRARY_PATH ."/templateFunctions.php");
+require_once(LIBRARY_PATH ."/templateFunctions.php");
 // renderPage();
 ?>
