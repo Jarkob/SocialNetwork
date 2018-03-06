95deca18ae90e81e30158122dc194b0714246d70
Jakob Bussas <jakobbu@gmx.de>
2018-03-06 14:05:03 +0100

    running again

--- SocialNetwork/resources/config.php
+++ SocialNetwork/resources/config.php
@@ -38,15 +38,15 @@ if($_SERVER['REMOTE_ADDR']=='127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '::1') {
 	define('ENV', 'local');
 }
 echo $_SERVER['REMOTE_ADDR'];
-//if(ENV == 'local') {
+if(ENV == 'local') {
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-	// sql::connect($dbconfig["local"]["host"], $dbconfig["local"]["username"], $dbconfig["local"]["password"], $dbconfig["local"]["dbname"]);
-//} else {
-// 	ini_set("error_reporting", "true");
-// 	error_reporting(E_ALL|E_STRICT);
+	sql::connect($dbconfig["local"]["host"], $dbconfig["local"]["username"], $dbconfig["local"]["password"], $dbconfig["local"]["dbname"]);
+} else {
+	ini_set("error_reporting", "true");
+	error_reporting(E_ALL|E_STRICT);
 	sql::connect($dbconfig["azure"]["host"], $dbconfig["azure"]["username"], $dbconfig["azure"]["password"], $dbconfig["azure"]["dbname"]);
-// }
+}
 
 ?>
