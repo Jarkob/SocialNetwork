a9f7c40e27d69893cda6db0415e8adeefbb05846
Jakob Bussas <jakobbu@gmx.de>
2018-03-06 13:53:13 +0100

    debug

--- SocialNetwork/resources/classes/log.php
+++ SocialNetwork/resources/classes/log.php
@@ -9,9 +9,11 @@ class log
 		$referrer = getenv('HTTP_REFERER');
 		$query = getenv('QUERY_STRING');
 
-		$sql = "INSERT INTO access_log (ip, browser, referrer, query) VALUES (:ip, :userAgent, :referrer, :query)";
-		$params = array(":ip" => $ip, ":userAgent" => $userAgent, ":referrer" => $referrer, ":query" => $query);
-		sql::exe($sql, $params);
+		echo "in logAccess";
+
+		// $sql = "INSERT INTO access_log (ip, browser, referrer, query) VALUES (:ip, :userAgent, :referrer, :query)";
+		// $params = array(":ip" => $ip, ":userAgent" => $userAgent, ":referrer" => $referrer, ":query" => $query);
+		// sql::exe($sql, $params);
 	}
 }
 ?>
